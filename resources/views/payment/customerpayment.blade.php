83cda11500167fb26ab5228845f188fe1e7d1557
salwaalnnn <salwaalnnn@student.telkomuniversity.ac.id>
2024-04-08 12:27:44 +0700

    DEL-15
    update blade customer payment

--- resources/views/payment/customerpayment.blade.php
+++ resources/views/payment/customerpayment.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pembayaran</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
@@ -30,7 +30,7 @@
                 <input type="number" name="total_amount" id="total_amount" class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="proof_of_payment">Unggah Bukti Pembayaran</label>
+                <label for="proof_of_payment">Unggah Bukti Pembayaran</label> <br>
                 <input type="file" name="proof_of_payment" id="proof_of_payment" class="form-control-file" required>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
