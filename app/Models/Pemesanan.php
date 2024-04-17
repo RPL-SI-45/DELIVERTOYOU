f9d7c91c16ebd6f7b2639d670b6095eb17615ea2
Iqmal Nuriadi <106952127+iqmlcaiera15@users.noreply.github.com>
2024-04-17 23:59:37 +0700

    DEL-9

--- app/Models/Pemesanan.php
+++ app/Models/Pemesanan.php
@@ -12,24 +12,17 @@ class Pemesanan extends Model
 {
     use HasFactory;
     protected $table = 'pemesanan';
-    
-}
-
     protected $guarded =[];
     
-    public function payment()
-    {
-        return $this->hasOne(Payment::class);
-    }
-    public function pesananmasuk() { 
-        return $this->belongsTo(PesananMasuk::class, 'pesananmasuk_id'); 
-    }
-    // public function payment()
-    // {
-    //     return $this->hasOne(Payment::class);
-    // }
-    // public function pesananmasuk() { 
-    //     return $this->belongsTo(PesananMasuk::class, 'pesananmasuk_id'); 
-    // }
-}
 
+
+    
+public function payment()
+    {return $this->hasOne(Payment::class);
+    
+    }
+public function pesananmasuk() { 
+    return $this->belongsTo(PesananMasuk::class, 'pesananmasuk_id'); 
+    }
+
+}
