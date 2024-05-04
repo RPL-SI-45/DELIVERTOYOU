9d6cf85748ce99f8c49c609469507c1494b45d87
salwaalnnn <salwaalnnn@student.telkomuniversity.ac.id>
2024-05-04 08:22:59 +0700

    DEL-10

--- app/Models/pemesanan.php
+++ app/Models/pemesanan.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\OrderController;
 
-class pemesanan extends Model
+class Pemesanan extends Model
 {
     use HasFactory;
     protected $table = 'pemesanan';
+    protected $guarded =[];
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+    public function pesananmasuk() { 
+        return $this->belongsTo(PesananMasuk::class, 'pesananmasuk_id'); 
+    }
 }
